1b6b46cc53ac0942694235388691a3871a1018c0
Dağhan Sever <daghansever@gmail.com>
2025-01-21 20:47:13 +0300

    separation of listeners v1.0
    new thread gets in the way of main logic, need to separate incoming data somehow also?

--- client.py
+++ client.py
@@ -8,18 +8,35 @@ import threading
 
 # LOGIC #
 # Client can only receive after client sends a message. need to separate listener logic.
+
+def expect_updates(client_socket):
+  print(f"Expecting updates from server.")
+  try:
+    while True:
+      data = client_socket.recv(1024).decode('utf-8')
+      if not data:
+        break
+      print(f"Received from server: {data}")
+  except Exception as e:
+    print(f"Error with server {e}")
+  finally:
+    print(f"Connection with server closed.")
+    client_socket.close()
+
 def start_client(server_ip):
   PORT = 12332
   client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
   try:
     client_socket.connect((server_ip, PORT))
     print(f"Connected to server at {server_ip}:{PORT}")
-
+    # start new thread
+    response_handler = threading.Thread(target=expect_updates, args=(client_socket,))
+    response_handler.start()
     while True:
       message = input("Enter message to send (type 'exit' to quit): ")
       if message.lower() == 'exit':
         break
-
+      
       client_socket.send(message.encode('utf-8'))
       response = client_socket.recv(1024).decode('utf-8')
       print(f"Server responded: {response}")
