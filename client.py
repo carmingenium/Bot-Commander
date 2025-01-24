cedb5127f9c42f7a9ca6b021ff5b4f6496ddd47a
Dağhan Sever <daghansever@gmail.com>
2025-01-24 14:12:27 +0300

    logic change
    second listener removed.
    instead listener and sender systems separated.
    reasons and logic explained in comments

--- client.py
+++ client.py
@@ -6,22 +6,32 @@ import threading
 # when any other function is active, program should be a black screen with only results from active function and input of the client.
 # this UI should be on the client.
 
-# LOGIC #
-# Client can only receive after client sends a message. need to separate listener logic.
 
-def expect_updates(client_socket):
-  print(f"Expecting updates from server.")
+def listener(client_socket):
+  print(f"Starting listener.")
   try:
     while True:
-      data = client_socket.recv(1024).decode('utf-8')
-      if not data:
-        break
-      print(f"Received from server: {data}")
+      response = client_socket.recv(1024).decode('utf-8')
+      print(f"Server sent: {response}")
   except Exception as e:
-    print(f"Error with server {e}")
+    print(f"Error with receiving a message: {e}")
   finally:
-    print(f"Connection with server closed.")
+    print(f"Client socket closed.")
     client_socket.close()
+
+def send_message(client_socket):
+  print(f"Starting message sending thread.")
+  try:
+    while True:
+      message = input("Enter message to send (type 'exit' to quit): ")
+      if message.lower() == 'exit':
+        client_socket.close()
+      client_socket.send(message.encode('utf-8'))
+      print(f"Message sent: {message}")
+  except Exception as e:
+    print(f"Error with sending a message: {e}")
+  finally:
+    print(f"Message sending thread closed.")
 
 def start_client(server_ip):
   PORT = 12332
@@ -29,17 +39,14 @@ def start_client(server_ip):
   try:
     client_socket.connect((server_ip, PORT))
     print(f"Connected to server at {server_ip}:{PORT}")
-    # start new thread
-    response_handler = threading.Thread(target=expect_updates, args=(client_socket,))
+    # start message sending thread:
+    #   to separate the listening and sending systems.
+    #   because client can only receive after client sends a message if they are combined.
+    #   thats would be a problem because server will keep sending updates on bot updates etc.
+    response_handler = threading.Thread(target=send_message, args=(client_socket,))
     response_handler.start()
-    while True:
-      message = input("Enter message to send (type 'exit' to quit): ")
-      if message.lower() == 'exit':
-        break
-      
-      client_socket.send(message.encode('utf-8'))
-      response = client_socket.recv(1024).decode('utf-8')
-      print(f"Server responded: {response}")
+    listener(client_socket)
+
   except Exception as e:
     print(f"Error connecting to server: {e}")
   finally:
